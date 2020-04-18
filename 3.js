638cafe7025cb9cb574d91f5fcc1674d5a0ebc33
FIRST_NAME LAST_NAME <elcoputra94@gmail.com>
2020-04-18 13:33:55 +0700

    3 beres

---
+++ 3.js
@@ -0,0 +1,19 @@
+// jalanin lewat console, bukan debug
+function cetak_gambar(target) {
+    let pattern1 = "* ";
+    let pattern2 = "= ";
+
+    for (let row = 1; row <= target; row++) {
+        
+        for (let column = 1; column <= target; column++) {
+            if (row === 1 || column === 1 || row=== target || column === target || row === target/2+0.5) {
+                process.stdout.write(pattern1);
+            }
+            else {
+                process.stdout.write(pattern2);
+            }
+        }
+        console.log("");
+    }
+}
+cetak_gambar(5);
